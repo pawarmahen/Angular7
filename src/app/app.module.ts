267e029b64cba3498e3a6d460a180b9581243740
Nilesh Bagde <nilesh.bagde@asentech.com>
2019-04-12 22:18:16 +0530

    contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './main/home/home.component';
 import { ProductComponent } from './main/product/product.component';
--- src/app/main/contacts/contacts.component.ts
+++ src/app/main/contacts/contacts.component.ts
@@ -8,16 +8,27 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class ContactsComponent implements OnInit {
 	exForm: FormGroup;
+	submitted = false;
 
 	constructor(private fb: FormBuilder) {
 		this.exForm = this.fb.group({
 			firstName: [ '', Validators.required ],
-			emailAddress: [ '', Validators.required ]
+			emailAddress: [ '', Validators.required, Validators.email],
+			subject: ['', Validators.required],
+			message: ['', Validators.required] 
 		});
 	}
 
 	get f() {
 		return this.exForm.controls;
+	}
+	onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.exForm.invalid) {
+            return;
+		}
 	}
 
 	ngOnInit() {}
